0fb7d4778161b39aa7928c8dbdc286f9645fbc61
MityaRimsky <qxps@mail.ru>
2025-05-20 08:06:32 +0300

    update llama4scout

--- openrouter_api.py
+++ openrouter_api.py
@@ -17,7 +17,7 @@ class OpenRouterAPI:
             "Content-Type": "application/json"
         }
     
-    def generate_response(self, prompt, context=None, model="meta-llama/llama-4-maverick:free"):
+    def generate_response(self, prompt, context=None, model="meta-llama/llama-4-scout:free"):
         """
         Генерирует ответ на основе промпта и контекста с использованием указанной модели.
         
